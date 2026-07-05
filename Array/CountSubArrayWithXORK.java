302ddd868b0d1b27f6d2ce0c447aee4c155c1fae
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-07-05 18:49:08 +0530

    Add files via upload

---
+++ Array/CountSubArrayWithXORK.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+/**
+ * CountSubArrayWithXORK
+ */
+public class CountSubArrayWithXORK {
+public static int bruteForce(int nums[],int k){
+    int count = 0;
+    for(int i=0;i<nums.length;i++){
+        int xor=0;
+        for(int j=i;j<nums.length;j++){
+            xor=xor^nums[j];
+            if(xor==k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+public static int optimalApproach(int nums[],int k ){
+    HashMap <Integer,Integer> map=new HashMap<>();
+    map.put(0, 1);
+    int xor=0;
+    int count=0;
+    for(int i :nums){
+        xor=xor^i;
+        int required=k^xor;
+        if(map.containsKey(required)){
+            count += map.get(required);
+        }
+            map.put(xor, map.getOrDefault(xor, 0)+1);
+        
+    }
+    return count;
+
+}
+    public static void main(String[] args) {
+        int nums[]={4,2,2,6,4};
+        System.out.println(bruteForce(nums, 6));
+        System.out.println(optimalApproach(nums, 6));
+    }
+}
