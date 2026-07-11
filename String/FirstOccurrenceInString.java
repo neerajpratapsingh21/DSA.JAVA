487513d1832adf3c614cb4b9a21146a5a66a7bb7
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-07-11 18:08:07 +0530

    Add files via upload

---
+++ String/FirstOccurrenceInString.java
@@ -0,0 +1,23 @@
+package string;
+
+public class FirstOccurrenceInString {
+    public static int firstOccurrence(String haystack , String needle){
+        int n=haystack.length();
+        int m=needle.length();
+        if(n<m)  return -1;
+        for(int i=0;i<n-m+1;i++){
+            int j=0;
+            while(j<m && haystack.charAt(i+j) == needle.charAt(j)){
+                j++;
+            }
+            if(j==m){
+              return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println(firstOccurrence("sadbutsad", ""));
+        
+    }
+}
