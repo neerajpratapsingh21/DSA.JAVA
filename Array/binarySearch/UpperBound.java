9da8f77019ed0235337759cc19c93ffc75ce1a9b
Neeraj baghel <neerajbaghel7505878012@gmail.com>
2026-07-19 15:04:12 +0530

    Add files via upload

---
+++ Array/binarySearch/UpperBound.java
@@ -0,0 +1,23 @@
+package binarysearch;
+
+public class UpperBound {
+  public static int upperbound(int arr[],int x){
+     int ans=arr.length;
+   int low = 0;
+   int high = arr.length-1;
+   while(low<=high){
+    int mid = (low + high)/2;
+    if(arr[mid]>x){
+        ans=mid;
+        high=mid-1;
+    }else{
+        low = mid +1;
+    }
+   }
+   return ans;
+  }
+      public static void main(String[] args) {
+        int arr[]={2,3,6,7,8,8,11,11,11,12};
+        System.out.println(upperbound(arr, 11));
+    }
+}
