66f1b82df04d196f8633b6194dea2f7d71db49b0
Neeraj Pratap Singh <neerajbaghel7505878012@gmail.com>
2026-08-07 23:14:23 +0530

    Add files via upload

---
+++ Array/binarySearch/PeakElement.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+public class PeakElement {
+    public static int bruteForce(int arr[]){
+          for(int i=0;i<arr.length;i++){
+            if((i==0 || arr[i-1]<arr[i])&& (i==arr.length-1 || arr[i+1]<arr[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int optimalSolution(int arr[]){
+        int n=arr.length;
+        if(n==1)return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+        int low =1;
+        int high=arr.length-2;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(arr[mid-1]<arr[mid] &&  arr[mid+1]<arr[mid]){
+                return mid;
+            }else if(arr[mid]<arr[mid+1]){
+                low=mid +1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,10,13,7};
+        System.out.println(optimalSolution(arr));
+        
+    }
+}
