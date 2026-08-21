6443f5c138076069788024c64564353af0f76083
Neeraj Pratap Singh <neerajbaghel7505878012@gmail.com>
2026-08-21 22:57:13 +0530

    Add files via upload

---
+++ LinkedList/RemoveNthNodeFromEndOfLL.java
@@ -0,0 +1,19 @@
+package singlyLinkedList;
+
+public class RemoveNthNodeFromEndOfLL {
+     static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+        Node(int data,Node next){
+            this.data=data;
+            this.next=next;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
