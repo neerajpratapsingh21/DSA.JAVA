bbad93c72b741ea93ba5ba5df354d8b1baa3ae55
Neeraj Pratap Singh <neerajbaghel7505878012@gmail.com>
2026-08-29 14:38:23 +0530

    Add files via upload

--- LinkedList/RemoveNthNodeFromEndOfLL.java
+++ LinkedList/RemoveNthNodeFromEndOfLL.java
@@ -1,5 +1,10 @@
 package singlyLinkedList;
+// Constraints:
 
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
 public class RemoveNthNodeFromEndOfLL {
      static class Node{
         int data;
@@ -13,7 +18,45 @@ public class RemoveNthNodeFromEndOfLL {
             this.next=next;
         }
     }
+    public static Node removeNthNode(Node head,int n){  
+        int size=0;
+        Node temp=head;
+        while(temp != null){
+            size++;
+            temp=temp.next;
+        }
+      if(n==size){
+        return head.next;
+      }
+      temp=head;
+ for(int i=1;i<size-n;i++){
+    temp=temp.next;
+ }
+ temp.next = temp.next.next;
+ return head;
+    }
+    public static Node optimalSoltion(Node head,int n){
+        Node fast=head;
+        for(int i=1;i<=n;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return head.next;
+        }
+        Node slow=head;
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
     public static void main(String[] args) {
-        
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+      
     }
 }
