a730b7bb7c10b73c062a96c6d95b4ceb9918bb58
neerajpratapsingh21 <neerajbaghel7505878012@gmail.com>
2026-09-12 21:58:38 +0530

    added

--- maths/PowerOfTwo.java
+++ maths/PowerOfTwo.java
@@ -2,15 +2,11 @@ package maths;
 
 public class PowerOfTwo {
      public static  boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        if(n==1) return true;
-        if(n%2==1) return false;
+        if(n==0) return false;
         while(n%2==0){
             n/=2;
         }
-        if(n<2) return true;
-
-        return false;
+        return  n==1;
     }
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(56));
