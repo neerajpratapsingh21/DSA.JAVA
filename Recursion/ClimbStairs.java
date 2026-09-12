c9d0ff255a82282938816520bdd993639309dc36
neerajpratapsingh21 <neerajbaghel7505878012@gmail.com>
2026-09-12 18:33:00 +0530

    updated

--- Recursion/ClimbStairs.java
+++ Recursion/ClimbStairs.java
@@ -1,11 +1,11 @@
 public class ClimbStairs {
-    public static int climbStairs(int n){
+    public static long climbStairs(long n){
       if(n<=3)return n;
-     int last= climbStairs(n-1);
-     int slast= climbStairs(n-2);
+     long last= climbStairs(n-1);
+     long slast= climbStairs(n-2);
        return last+slast;
     }
     public static void main(String[] args) {
-        System.out.println(climbStairs(5));
+        System.out.println(climbStairs(46));
     }
 }
