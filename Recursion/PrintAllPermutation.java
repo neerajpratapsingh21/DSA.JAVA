1c6e2184f91a1690a45af2578f2adde40c1aece4
neerajpratapsingh21 <neerajbaghel7505878012@gmail.com>
2026-09-15 21:52:32 +0530

    added

--- Recursion/PrintAllPermutation.java
+++ Recursion/PrintAllPermutation.java
@@ -5,8 +5,9 @@ public class PrintAllPermutation {
     
     public static List<List<Integer>> printPermutation(int arr[]){
         List<List<Integer>> ans=new ArrayList<>();
-        boolean map[]=new boolean[arr.length];
-        recursivePermutation(arr,new ArrayList<>(),ans,map);
+       // boolean map[]=new boolean[arr.length];
+       // recursivePermutation(arr,new ArrayList<>(),ans,map);
+permutation(arr, 0, ans);
         return  ans;
     }
     public static void recursivePermutation(int[] arr, List<Integer> list, List<List<Integer>> ans, boolean[] map) {
@@ -23,6 +24,29 @@ public class PrintAllPermutation {
                 map[i]=false;
             }
         }
+    }
+    public static  void permutation(int arr[],int ind, List<List<Integer>> ans){
+       if(ind == arr.length){
+    List<Integer> temp = new ArrayList<>();
+
+    for(int x : arr){
+        temp.add(x);
+    }
+
+    ans.add(temp);
+    return;
+}
+        for(int i=ind; i<arr.length;i++){
+         swap(ind, i, arr);
+
+         permutation(arr, ind+1, ans);
+         swap(ind, i, arr);
+        }
+    }
+    public static void swap(int left,int right,int arr[]){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
     }
     public static void main(String[] args) {
         int arr[]={3,1,2};
--- maths/CountPrime.java
+++ maths/CountPrime.java
@@ -1,11 +1,32 @@
 package maths;
 
+import java.util.Arrays;
+
 /**
  * CountPrime
  */
 public class CountPrime {
+     public static  int countPrimes(int n) {
+        if(n<=1) return 0;
+        int prime[]=new int[n];
+           Arrays.fill(prime,1);
+            for(int i=2;i<=Math.sqrt(n);i++){
+               if(prime[i]==1){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j]=0;
+                }
+               }
+            }
+            int count=0;
+            for(int i=2;i<n;i++){
+               if(prime[i]==1){
+                count++;
+               }
+            }
+        return count;
+    }
 public static void main(String[] args) {
-    
+    System.out.println(countPrimes(5));
 }
     
 }
